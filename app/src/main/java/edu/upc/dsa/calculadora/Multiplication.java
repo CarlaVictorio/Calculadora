c9cdaf0da61b522fde0e5222e634bdd8d0b1f4db
CarlaVictorio <101456362+CarlaVictorio@users.noreply.github.com>
2022-10-22 19:48:19 +0200

    final

---
+++ app/src/main/java/edu/upc/dsa/calculadora/Multiplication.java
@@ -0,0 +1,9 @@
+package edu.upc.dsa.calculadora;
+
+public class Multiplication implements Operations{
+
+    @Override
+    public double operation(double a, double b) {
+        return a*b;
+    }
+}
---
+++ app/src/main/java/edu/upc/dsa/calculadora/Operations.java
@@ -0,0 +1,6 @@
+package edu.upc.dsa.calculadora;
+
+public interface Operations {
+
+    public double operation (double a, double b);
+}
